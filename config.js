3d582c76f7517a56ddfc2086d38dd811d00db473
Charlie Pauvré <chpauvre@gmail.com>
2020-10-29 01:05:15 +0100

    init v2

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	MONGODB:
+		'mongodb+srv://charlie_pauvre:6nokkHFTb6CCEyX4@socialmediaapp.jqlk7.mongodb.net/main_database?retryWrites=true&w=majority',
+	SECRET_KEY: 'eijziuxaozuzpropzo'
+};
